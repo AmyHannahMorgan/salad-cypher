df84d89b16bb582c3d139b5863fb33177ebf5563
AmyHannahMorgan <amy.hannah.morgan@gmail.com>
2022-02-14 11:12:41 +0000

    +compute cyphertext, print for now

--- salad-cypher.py
+++ salad-cypher.py
@@ -8,17 +8,37 @@ x2 = 0
 x1 = 0
 x0 = 0
 
+trueI = 0
+
 output = ''
 
 for i in range(len(input)):
-    #calculate next step in sequence
-    x0 = x1 + x2
+    if(input[i] != ' '):
+        #calculate next step in sequence
+        x0 = x1 + x2
 
-    #if x0 is 0 set it to one to begin the sequence
-    if(x0 == 0):
-        x0 = 1
-    
-    #swap terms around for next calculation
-    x2 = x1
-    x1 = x0
-    
+        #if x0 is 0 set it to one to begin the sequence
+        if(x0 == 0):
+            x0 = 1
+        
+        #swap terms around for next calculation
+        x2 = x1
+        x1 = x0
+
+        charIndex = alphabet.index(input[i])
+        cypherIndex = 0
+
+        if(trueI % 2 == 0):
+            cypherIndex = (charIndex + x0) % len(alphabet)
+        elif(x0 > charIndex):
+            cypherIndex = len(alphabet) - (abs(charIndex - x0) % len(alphabet))
+        else:
+            cypherIndex = charIndex - x0
+
+        output = output + alphabet[cypherIndex]
+        trueI = trueI + 1
+    else:
+        output = output + ' '
+
+print(output)
+        
