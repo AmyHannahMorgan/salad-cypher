6b16e9105a5bbfef188c8b0555cdff575a66f941
AmyHannahMorgan <amy.hannah.morgan@gmail.com>
2022-02-14 12:19:34 +0000

    +add decoding support

--- salad-cypher.py
+++ salad-cypher.py
@@ -1,11 +1,31 @@
 from sys import argv
 
+def codec(flag, index, length, fibb, fibbCount):
+    if(flag == 'e'):
+        if(fibbCount % 2 == 0):
+            return (index + fibb) % length
+        elif(fibb > index):
+            return (length - 1) - (abs(index - fibb) % length)
+        else :
+            return index - fibb
+    else: 
+        if(fibbCount % 2 == 0):
+            if(fibb > index):
+                return (length - 1) - (abs(index - fibb + 1) % length)
+            else :
+                return index - fibb
+        else: 
+            if(index + fibb > length):
+                return (index + fibb + 1) % length
+            else:
+                return (index + fibb) % length 
+
 alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 cypherbet = None
 
-if(argv[1]):
-    cypherbet = list(argv[1].upper()) #some funky issues might occur if cypher key has duplicate characters like "salad" but code should handle it fine
+if(argv[2]):
+    cypherbet = list(argv[2].upper()) #some funky issues might occur if cypher key has duplicate characters like "salad" but code should handle it fine
     for char in alphabet:
         if char not in cypherbet:
             cypherbet.append(char)
@@ -38,14 +58,14 @@ for i in range(len(input)):
         x1 = x0
 
         charIndex = cypherbet.index(input[i])
-        cypherIndex = 0
+        cypherIndex = codec(argv[1], charIndex, len(cypherbet), x0, trueI)
 
-        if(trueI % 2 == 0):
-            cypherIndex = (charIndex + x0) % len(cypherbet)
-        elif(x0 > charIndex):
-            cypherIndex = (len(cypherbet) - 1) - (abs(charIndex - x0) % len(cypherbet))
-        else:
-            cypherIndex = charIndex - x0
+        # if(trueI % 2 == 0):
+        #     cypherIndex = (charIndex + x0) % len(cypherbet)
+        # elif(x0 > charIndex):
+        #     cypherIndex = (len(cypherbet) - 1) - (abs(charIndex - x0) % len(cypherbet))
+        # else:
+        #     cypherIndex = charIndex - x0
 
         output = output + cypherbet[cypherIndex]
         trueI = trueI + 1
